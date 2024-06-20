61b97fc8db397fb0f7a659b0b3db19722c9d839b
dynamic2code <dynamicdesingns@gmail.com>
2024-06-20 13:34:34 +0300

    fix: security fix

--- Donation_tracker/Donation_tracker/settings.py
+++ Donation_tracker/Donation_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(4#__#r)!&rrnq#b&%q!v^e21f6@s@l5^2z+a6kw)#k$m9e!3a'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
